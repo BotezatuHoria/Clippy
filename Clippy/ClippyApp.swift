e32208b03b07a32dfe86f9c9936e0c769c7b637d
Botezatu Horia <horia12btz@gmail.com>
2024-08-10 17:43:06 +0300

    Initial Commit

---
+++ Clippy/ClippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyApp.swift
+//  Clippy
+//
+//  Created by Horia Botezatu on 10.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
